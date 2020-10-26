dbf9fba0f25a0f753cde61201fedd6283369524c
nhat51 <65001571+nhat51@users.noreply.github.com>
2020-10-26 09:09:59 +0700

    fix

--- Pratice/ex2/TestFlight.java
+++ Pratice/ex2/TestFlight.java
@@ -2,7 +2,7 @@ package Pratice.ex2;
 
 public class TestFlight {
     public static void main(String[] args) {
-        Flight f1 = new Flight(857,"Toronto");
-        f1.Display();
+        Flight g1 = new Flight(857,"Toronto");
+        g1.Display();
     }
 }
